7fa9c41d4e5174972768d5f6e7878a05bdceae81
Fabio <fabiobarreto208@gmail.com>
2020-01-27 02:45:19 -0300

    Adicionando tabela

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+	displayedColumns: string[] = ['placa','ponto','tempo'];
+
   title = 'mobi7-frontend';
+  dados = [
+	{placa: 'ASD 3D40', ponto: '1', tempo: '10'},
+	{placa: 'QWE 3D40', ponto: '2', tempo: '11'},
+	{placa: 'ZXC 3D40', ponto: '3', tempo: '12'}];
+	
+	selected = 'option2';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MatInputModule,
+  MatPaginatorModule,
+  MatProgressSpinnerModule,
+  MatSortModule,
+  MatIconModule,
+  MatTableModule,
+  MatButtonModule,
+  MatCardModule,
+  MatGridListModule
+} from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+	MatTableModule,
+    MatPaginatorModule,
+    MatProgressSpinnerModule,
+    MatSortModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
